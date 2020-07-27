23ecbb4e78c870d3cf2495b7311601b18589caed
Rosingsun <476348131@qq.com>
2020-07-27 15:48:17 +0800

    个人中心大致排版完成

--- demo04/App/PersonalCenter/Eline.js
+++ demo04/App/PersonalCenter/Eline.js
@@ -2,73 +2,90 @@ import React, { Component } from 'react';
 import Swiper from 'react-native-swiper';
 import { Text, View, StyleSheet, FlatList, Image, Alert } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import { Transition } from 'react-native-reanimated';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 var userDate = [
-  { key: "日落的色彩给向日葵镀上了一层金色，相间的小道" },
-  { key: "2" },
-  { key: "3" },
+  {
+    key: "日落的色彩给向日葵镀上了一层金色，相间的小道日落的色彩给向日葵镀上了一层金色，相间的小道日落的色彩给向日葵镀上了一层金色，相间的小道日落的色彩给向日葵镀上了一层金色，相间的小道",
+
+  },
+  { key: "日落的色彩给向日葵镀上了一层金色，相间的小道02" },
+  { key: "日落的色彩给向日葵镀上了一层金色，相间的小道03" },
 ]
-function userContainBox() {
-
-}
-
-function SwiperMainContainer(Mitem) {
+var imgUrl = [
+  {
+    key: "1",
+    imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
+  }, {
+    key: "2",
+    imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
+  }, {
+    key: "3",
+    imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
+  },
+]
+function SwiperMainContainer(item) {
   return (
     <View style={[styles.mainBox]}>
-      {/* 轮播图 */}
       <View style={[styles.swiperPic]}>
-        {/* 做轮播用的flatlist */}
-        <FlatList
-          horizontal={true}
-          pagingEnabled={true}
-          showsHorizontalScrollIndicator={false}
-          data={[
-            {
-              key: "1",
-              imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
-            }, {
-              key: "2",
-              imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
-            }, {
-              key: "3",
-              imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
-            },
-          ]}
-          renderItem={({ item }) =>
-            <View style={{ width: 400, height: 200, borderRadius: 3 }}>
-              <Image style={{ height:'90%', width: "100%" }} source={{ uri: item.imgUrl }} />
-              <View style={{ width: 10, height: 10,marginLeft:10, backgroundColor: "red"}}>
+        <Swiper style={{ height: '100%' }}
+          autoplay={true}
+          dotColor="#999999"
+          activeDotColor="#43949B"
+          dotStyle={{
+            height: 5,
+            width: 5,
+          }}
+          activeDotStyle={{
+            height: 5,
+            width: 5,
+          }}
+        >
+          {
+            imgUrl.map((item) => {
+              return (
+                <View style={{ height: '100%', width: "100%" }}>
+                  <Image style={{ height: '100%', width: '100%', backgroundColor: "yellow" }} source={{ uri: item.imgUrl }} />
+                </View>
+              )
+            })
+          }
+        </Swiper>
+      </View>
+      <View style={{ width: '100%', backgroundColor: "#fff", paddingVertical: 10, paddingHorizontal: 10 }}>
+        <View>
+          <Text style={[styles.userWord]}>{item.key}</Text>
+          <View style={{ width: '100%', flexDirection: "row", paddingBottom: 10, }}>
+            <Text style={[styles.activeSign]}>#种豆南山下</Text>
+          </View>
+          <View style={{ width: '100%', height: 30, flexDirection: "row", justifyContent: "space-between" }}>
+            <View>
+              <MaterialCommunityIcons name={'share-circle'} size={30} color={'#484848'} />
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons name={'md-location-sharp'} size={30} color={'#484848'} />
+              <Ionicons name={'md-location-sharp'} size={30} color={'#484848'} />
+              <View style={{flexDirection:"row",alignItems:"center",borderWidth:1,borderColor:"#000000",borderRadius:20,paddingHorizontal:5}}>
+                <AntDesign name={'like2'} size={20} color={'#484848'} />
+                <Text style={{fontSize:12}}>123</Text>
               </View>
             </View>
-          }
-        />
-
-      </View>
-      <View style={{ height: 125, width: '100%', backgroundColor: "#fff" }}>
-        <View>
-          <Text style={{ letterSpacing: 1, color: "#197178", fontSize: 12 }}>{Mitem.key}</Text>
+          </View>
         </View>
       </View>
     </View>
   )
-
-
 }
 export default function Eline() {
   return (
-    // <View style={{flex:0.9}}>
-    // <ScrollView style={{backgroundColor: "#43949B" ,height:'100%'}}
-    // scrollEnabled={false}
-    // showsVerticalScrollIndicator={false}
-    // onScrollEndDrag={()=>{
-    //   Alert.alert("!11");
-    // }}
-    // >
-      <View style={[styles.container]}>
-        {/* <ScrollView style={{ width: '100%',height:'10%', backgroundColor: "#FFFfff00" }}
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ backgroundColor: "#43949B28", height: 10 }}
+        scrollEnabled={true}
         showsVerticalScrollIndicator={false}
-        scrollEnabled={false}
-        > */}
+      >
+        <View style={[styles.container]}>
           {
             userDate.map((item) => {
               return (
@@ -76,28 +93,40 @@ export default function Eline() {
               )
             })
           }
-        {/* </ScrollView> */}
-      </View>
-    // </ScrollView>
-    // </View>
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     // width: '100%',
-    // height: '60%',
+    height: '60%',
     backgroundColor: "#43949B",
     padding: 10,
   },
   mainBox: {
     width: '100%',
     marginTop: 10,
-    height: 310,
+    // height: 310,
     backgroundColor: "#fff",
     borderRadius: 3,
   },
   swiperPic: {
     width: '100%',
-    height: '60%', backgroundColor: "#fff",
+    height: 200,
+    backgroundColor: "#000",
+  },
+  userWord: {
+    letterSpacing: 1,
+    color: "#197178",
+    fontSize: 12,
+    paddingBottom: 5,
+  },
+  activeSign: {
+    padding: 3,
+    backgroundColor: "#43949B28",
+    fontSize: 12,
+    color: "#197178",
   },
 })
--- demo04/App/PersonalCenter/Eshouchang.js
+++ demo04/App/PersonalCenter/Eshouchang.js
@@ -1,9 +1,132 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-export default class Eshouchang extends Component {
-  render() {
-    return (
-      <Text>Hello, I am your cat!</Text>
-    );
-  }
+import Swiper from 'react-native-swiper';
+import { Text, View, StyleSheet, FlatList, Image, Alert } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Transition } from 'react-native-reanimated';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+var userDate = [
+  {
+    key: "日落的色彩给向日葵镀上了一层金色，相间的小道日落的色彩给向日葵镀上了一层金色，相间的小道日落的色彩给向日葵镀上了一层金色，相间的小道日落的色彩给向日葵镀上了一层金色，相间的小道",
+
+  },
+  { key: "日落的色彩给向日葵镀上了一层金色，相间的小道02" },
+  { key: "日落的色彩给向日葵镀上了一层金色，相间的小道03" },
+]
+var imgUrl = [
+  {
+    key: "1",
+    imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
+  }, {
+    key: "2",
+    imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
+  }, {
+    key: "3",
+    imgUrl: "http://pic.51yuansu.com/pic3/cover/03/99/56/5f1aa3a3387aa_610.jpg!/fw/260/quality/90/unsharp/true/compress/true",
+  },
+]
+function SwiperMainContainer(item) {
+  return (
+    <View style={[styles.mainBox]}>
+      <View style={[styles.swiperPic]}>
+        <Swiper style={{ height: '100%' }}
+          autoplay={true}
+          dotColor="#999999"
+          activeDotColor="#43949B"
+          dotStyle={{
+            height: 5,
+            width: 5,
+          }}
+          activeDotStyle={{
+            height: 5,
+            width: 5,
+          }}
+        >
+          {
+            imgUrl.map((item) => {
+              return (
+                <View style={{ height: '100%', width: "100%" }}>
+                  <Image style={{ height: '100%', width: '100%', backgroundColor: "yellow" }} source={{ uri: item.imgUrl }} />
+                </View>
+              )
+            })
+          }
+        </Swiper>
+      </View>
+      <View style={{ width: '100%', backgroundColor: "#fff", paddingVertical: 10, paddingHorizontal: 10 }}>
+        <View>
+          <Text style={[styles.userWord]}>{item.key}</Text>
+          <View style={{ width: '100%', flexDirection: "row", paddingBottom: 10, }}>
+            <Text style={[styles.activeSign]}>#种豆南山下</Text>
+          </View>
+          <View style={{ width: '100%', height: 30, flexDirection: "row", justifyContent: "space-between" }}>
+            <View>
+              <MaterialCommunityIcons name={'share-circle'} size={30} color={'#484848'} />
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <Ionicons name={'md-location-sharp'} size={30} color={'#484848'} />
+              <Ionicons name={'md-location-sharp'} size={30} color={'#484848'} />
+              <View style={{flexDirection:"row",alignItems:"center",borderWidth:1,borderColor:"#000000",borderRadius:20,paddingHorizontal:5}}>
+                <AntDesign name={'like2'} size={20} color={'#484848'} />
+                <Text style={{fontSize:12}}>123</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
+export default function Eshouchang() {
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ backgroundColor: "#43949B28", height: 10 }}
+        scrollEnabled={true}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={[styles.container]}>
+          {
+            userDate.map((item) => {
+              return (
+                SwiperMainContainer(item)
+              )
+            })
+          }
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    // width: '100%',
+    height: '60%',
+    backgroundColor: "#43949B",
+    padding: 10,
+  },
+  mainBox: {
+    width: '100%',
+    marginTop: 10,
+    // height: 310,
+    backgroundColor: "#fff",
+    borderRadius: 3,
+  },
+  swiperPic: {
+    width: '100%',
+    height: 200,
+    backgroundColor: "#000",
+  },
+  userWord: {
+    letterSpacing: 1,
+    color: "#197178",
+    fontSize: 12,
+    paddingBottom: 5,
+  },
+  activeSign: {
+    padding: 3,
+    backgroundColor: "#43949B28",
+    fontSize: 12,
+    color: "#197178",
+  },
+})
