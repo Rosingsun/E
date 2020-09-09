8ef15d0529bdf475277b13e461c3589ea796fcb9
Rosingsun <476348131@qq.com>
2020-09-10 02:46:09 +0800

    detailsChange

--- demo04/App/News/trade.js
+++ demo04/App/News/trade.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
@@ -16,78 +16,109 @@ import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 const { width, scale } = Dimensions.get("window");
 const biLi = width * scale / 1125;
-const trade = ({ navigation }) => {
-/*
-      tfinish 0为未交易，1为完成交易
-      MyJob   0为，我未守约，1为完守约
-      OtherJob 0 为未完成，1为完成
-    */
-  function tradeBox(finish,MyJob,OtherJob, name,OplaceName,MplaceName) { 
-   //交易是否已完成
-    function finishYet(finish){
-      if(finish==1){
-      return(
-<View style={{ alignItems: "center", flexDirection: "row", width: '30%', justifyContent: "flex-end" }}>
-                <AntDesign name={'sync'} size={20} color={'#999999'} />
-                <Text style={{ fontSize: 10, color: "#484848", paddingLeft: 10 }}>已完成</Text>
-              </View>
-      )}else{
-        return(
-        <View style={{ alignItems: "center", flexDirection: "row", width: '30%', justifyContent: "flex-end" }}>
-                <Entypo name={'back-in-time'} size={20} color={'#999999'} />
-                <Text style={{ fontSize: 10, color: "#484848", paddingLeft: 10 }}>未完成</Text>
-              </View>
-        )
-      }
-    }
-    // 自己是否已经完成
-    function myfinishYet(finish,MyJob){
-      //交易完成，自己已经履约
-      if(finish==1&&MyJob==1){
-        return(
-          <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent: "flex-end" }}>
-          <Text style={{ fontSize: 12, color: "#999999", paddingRight: 5, }}>已履约</Text>
-          <AntDesign name={'checkcircleo'} size={12} color={'#999999'} />
-        </View>
-        //交易完成，自己未履约
-        )}else if(finish==1&&MyJob==0){
-          return(
-            <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent: "flex-end" }}>
-            <Text style={{ fontSize: 12, color: "#999999", paddingRight:3, }}>已违约</Text>
-            <EvilIcons name={'close-o'} size={20} color={'red'} />
-          </View>
-          // 交易未完成
-          )}else if(finish==0){
-            return(
-              <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent: "flex-end" }}>
-              <Text style={{ fontSize: 12, color: "#fff",backgroundColor:"#6C9575",marginRight:5,paddingHorizontal:8,borderRadius:10 ,paddingVertical:3}}>我已履约</Text>
+class trade extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stateName: "",
+    };
+  }
+  /*
+        tfinish 0为未交易，1为完成交易
+        MyJob   0为，我未守约，1为完守约
+        OtherJob 0 为未完成，1为完成
+      */
+  render() {
+    function tradeBox(finish, MyJob, OtherJob, name, OplaceName, MplaceName) {
+      //交易是否已完成
+      function finishYet(finish) {
+        if (finish == 1) {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: '30%', justifyContent: "flex-end" }}>
+              <AntDesign name={'sync'} size={20} color={'#999999'} />
+              <Text style={{ fontSize: 10, color: "#484848", paddingLeft: 10 }}>已完成</Text>
             </View>
-            )}
-    }
-        // 他人否已经完成
-        function OtherYet(finish,OtherJob){
-          //交易完成，他人也完成
-          if(finish==1&&OtherJob==1){
-            return(
-              <View style={{ alignItems: "center", flexDirection: "row", width: "25%" }}>
-                <Text style={{ fontSize: 12, color: "#999999" }}>已履约</Text>
-                <AntDesign name={'checkcircleo'} size={12} color={'#999999'} />
-              </View>
-              // 交易完成，他人未履约
-            )}else if(finish==1&&OtherJob==0){
-              return(
-                <View style={{ alignItems: "center", flexDirection: "row", width: "25%" }}>
-                <Text style={{ fontSize: 12, color: "#999999" }}>已违约</Text>
-                <EvilIcons name={'close-o'} size={20} color={'red'} />
-              </View>
-              // 交易未完成
-              )}else if(finish==0){
-                return(
-                  <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent:"flex-start" }}>
-                  <Text style={{ fontSize: 12, color: "#fff",backgroundColor:"#6C9575",marginRight:5,paddingHorizontal:8,borderRadius:10 ,paddingVertical:3}}>Ta已履约</Text>
-                </View>
-                )}
+          )
+        } else {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: '30%', justifyContent: "flex-end" }}>
+              <Entypo name={'back-in-time'} size={20} color={'#999999'} />
+              <Text style={{ fontSize: 10, color: "#484848", paddingLeft: 10 }}>未完成</Text>
+            </View>
+          )
         }
+      }
+      function textType(finish) {
+        if (finish == 1) {
+          return (
+            <View>
+              <Text style={[styles.sealCost]}>订单评价</Text>
+            </View>
+          )
+        } else {
+          return (
+            <View>
+              <Text style={[styles.sealCost]}>取消订单</Text>
+            </View>
+          )
+        }
+      }
+      // 自己是否已经完成
+      function myfinishYet(finish, MyJob) {
+        //交易完成，自己已经履约
+        if (finish == 1 && MyJob == 1) {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent: "flex-end" }}>
+              <Text style={{ fontSize: 12, color: "#999999", paddingRight: 5, }}>已履约</Text>
+              <AntDesign name={'checkcircleo'} size={12} color={'#999999'} />
+            </View>
+            //交易完成，自己未履约
+          )
+        } else if (finish == 1 && MyJob == 0) {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent: "flex-end" }}>
+              <Text style={{ fontSize: 12, color: "#999999", paddingRight: 3, }}>已违约</Text>
+              <EvilIcons name={'close-o'} size={20} color={'red'} />
+            </View>
+            // 交易未完成
+          )
+        } else if (finish == 0) {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent: "flex-end" }}>
+              <Text style={{ fontSize: 12, color: "#fff", backgroundColor: "#6C9575", marginRight: 5, paddingHorizontal: 8, borderRadius: 10, paddingVertical: 3 }}>我已履约</Text>
+            </View>
+          )
+        }
+      }
+      // 他人否已经完成
+      function OtherYet(finish, OtherJob) {
+        //交易完成，他人也完成
+        if (finish == 1 && OtherJob == 1) {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: "25%" }}>
+              <Text style={{ fontSize: 12, color: "#999999" }}>已履约</Text>
+              <AntDesign name={'checkcircleo'} size={12} color={'#999999'} />
+            </View>
+            // 交易完成，他人未履约
+          )
+        } else if (finish == 1 && OtherJob == 0) {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: "25%" }}>
+              <Text style={{ fontSize: 12, color: "#999999" }}>已违约</Text>
+              <EvilIcons name={'close-o'} size={20} color={'red'} />
+            </View>
+            // 交易未完成
+          )
+        } else if (finish == 0) {
+          return (
+            <View style={{ alignItems: "center", flexDirection: "row", width: "25%", justifyContent: "flex-start" }}>
+              <Text style={{ fontSize: 12, color: "#fff", backgroundColor: "#6C9575", marginRight: 5, paddingHorizontal: 8, borderRadius: 10, paddingVertical: 3 }}>Ta已履约</Text>
+            </View>
+          )
+        }
+      }
+      //设置  <Text style={[styles.sealCost]}>订单评价</Text>
+
       return (
         <View style={[styles.tradeMainBox]}>
           {/* 第一个用户信息框 */}
@@ -111,7 +142,7 @@ const trade = ({ navigation }) => {
                   <Text style={{ fontSize: 15, color: "#999999", marginLeft: 10, }}>{OplaceName}</Text>
                 </View>
                 {
-                  myfinishYet(finish,MyJob)
+                  myfinishYet(finish, MyJob)
                 }
               </View>
             </View>
@@ -126,9 +157,9 @@ const trade = ({ navigation }) => {
           <View >
             {/* 成交详情 */}
             <View style={[styles.dealD]}>
-              
+
               {
-                OtherYet(finish,OtherJob)
+                OtherYet(finish, OtherJob)
               }
               <View style={{ width: '75%', height: 40, backgroundColor: "#EFEFEF", justifyContent: "center", alignItems: "flex-end" }}>
                 <Text style={{ fontSize: 15, color: "#999999", marginRight: 10 }}>{MplaceName}</Text>
@@ -137,7 +168,9 @@ const trade = ({ navigation }) => {
             {/* 用户信息 */}
             <View style={{ paddingHorizontal: 10, flexDirection: "row", justifyContent: "space-between", marginTop: 5 }}>
               <View style={{ alignItems: "center", flexDirection: "row", width: '50%', justifyContent: "space-around" }}>
-                <Text style={[styles.sealCost]}>取消订单</Text>
+                {
+                  textType(finish)
+                }
                 <Text style={[styles.dealEndChoice]}>投诉</Text>
                 <Text style={[styles.dealEndChoice]}>申述</Text>
               </View>
@@ -152,71 +185,72 @@ const trade = ({ navigation }) => {
           </View>
         </View>
       )
-  }
+    }
 
-  return (
-    <View style={[styles.container]}>
-      {/* flatlist渲染层从这开始 */}
-      <View style={{ paddingTop: 10, height: '100%', paddingBottom: 10 }}>
-        <FlatList
-          showsHorizontalScrollIndicator={false}
-          showsVerticalScrollIndicator={false}
-          data={[
-            {
-              name: 'JK&妹',
-              finish: 0,
-              MyJob:1,
-              OtherJob:0,
-              OplaceName:"杭州市宋城风景区",
-              MplaceName:"横店影视城",
-            },
-            {
-              name: 'JK&妹',
-              finish: 1,
-              MyJob:1,
-              OtherJob:0,
-              OplaceName:"杭州市宋城风景区",
-              MplaceName:"杭州市西湖风景区",
-            },
-            {
-              name: 'JK&妹',
-              finish: 0,
-              MyJob:1,
-              OtherJob:1,
-              OplaceName:"杭州市宋城风景区",
-              MplaceName:"杭州市西湖风景区",
-            },
-            {
-              name: 'JK&妹',
-              finish: 1,
-              MyJob:0,
-              OtherJob:1,
-              OplaceName:"杭州市宋城风景区",
-              MplaceName:"西溪国家湿地公园",
-            },
-            {
-              name: 'JK&妹',
-              finish: 1,
-              MyJob:0,
-              OtherJob:1,
-              OplaceName:"西溪国家湿地公园",
-              MplaceName:"杭州市西湖风景区",
-            },
-            {
-              name: 'JK&妹',
-              finish: 0,
-              MyJob:1,
-              OtherJob:1,
-              OplaceName:"杭州市宋城风景区",
-              MplaceName:"千岛湖风景区",
-            },
-          ]}
-          renderItem={({ item }) =>
-            tradeBox(item.finish,item.MyJob,item.OtherJob, item.name,item.OplaceName,item.MplaceName)
-          } />
+    return (
+      <View style={[styles.container]}>
+        {/* flatlist渲染层从这开始 */}
+        <View style={{ paddingTop: 10, height: '100%', paddingBottom: 10 }}>
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+            data={[
+              {
+                name: 'JK&妹',
+                finish: 0,
+                MyJob: 1,
+                OtherJob: 0,
+                OplaceName: "杭州市宋城风景区",
+                MplaceName: "横店影视城",
+              },
+              {
+                name: 'JK&妹',
+                finish: 1,
+                MyJob: 1,
+                OtherJob: 0,
+                OplaceName: "杭州市宋城风景区",
+                MplaceName: "杭州市西湖风景区",
+              },
+              {
+                name: 'JK&妹',
+                finish: 0,
+                MyJob: 1,
+                OtherJob: 1,
+                OplaceName: "杭州市宋城风景区",
+                MplaceName: "杭州市西湖风景区",
+              },
+              {
+                name: 'JK&妹',
+                finish: 1,
+                MyJob: 0,
+                OtherJob: 1,
+                OplaceName: "杭州市宋城风景区",
+                MplaceName: "西溪国家湿地公园",
+              },
+              {
+                name: 'JK&妹',
+                finish: 1,
+                MyJob: 0,
+                OtherJob: 1,
+                OplaceName: "西溪国家湿地公园",
+                MplaceName: "杭州市西湖风景区",
+              },
+              {
+                name: 'JK&妹',
+                finish: 0,
+                MyJob: 1,
+                OtherJob: 1,
+                OplaceName: "杭州市宋城风景区",
+                MplaceName: "千岛湖风景区",
+              },
+            ]}
+            renderItem={({ item }) =>
+              tradeBox(item.finish, item.MyJob, item.OtherJob, item.name, item.OplaceName, item.MplaceName)
+            } />
+        </View>
       </View>
-    </View>
-  )
+    )
+  }
 }
 export default trade;
 const styles = StyleSheet.create({
@@ -248,13 +282,12 @@ const styles = StyleSheet.create({
     borderColor: "#999999",
   },
   sealCost: {
-    width: "30%",
     fontSize: 10,
     textAlign: "center",
     borderRadius: 10,
     color: "#fff",
     backgroundColor: "#2F3843",
-    paddingHorizontal:2,
-    paddingVertical:2,
+    paddingHorizontal: 8,
+    paddingVertical: 3,
   }
 });
